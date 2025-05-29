c4b8d43e7cadebe89edecc4a6490ae762b7a8229
Jonathan Cavalcanti <54328913+qajonathanpoa@users.noreply.github.com>
2025-05-28 23:10:24 -0300

    Delete cypress-srbarriga-api directory

--- cypress-srbarriga-api/cypress/support/elements_page/login-page_elements.js
+++
@@ -1,8 +0,0 @@
-class LoginPageElements{
-    emailField = () =>  {return '.input-group [data-test=email]'}
-    passwordField = () =>  {return '.form-group [data-test=passwd]'}
-    entrarBtn = () =>  {return '.btn'}
-    bemVindoMessage = () =>  {return '.toast-message'}
-        
-}
-export default LoginPageElements;
